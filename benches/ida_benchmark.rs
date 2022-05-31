0084e4f44d5eff319a63674ea3b1c1988cb4cfb0
David Irvine <david.irvine@maidsafe.net>
2022-06-01 00:52:34 +0100

    test: Add throughput measure in benchmark

--- benches/ida_benchmark.rs
+++ benches/ida_benchmark.rs
@@ -1,5 +1,5 @@
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use rabin_ida::RabinIDA;
 
 pub fn ida_benchmark(c: &mut Criterion) {
@@ -13,6 +13,11 @@ let n = 7;
 //  let rec = sharer.reconstruct(shares[1..=k as usize].to_vec()).unwrap();
     c.bench_function("ida 1mb 5of7 create", |b| b.iter(|| sharer.share(black_box(data.clone()))));
     c.bench_function("ida 1mb 5of7 reconstruct", |b| b.iter(|| sharer.reconstruct(black_box(shares[1..=k as usize].to_vec())).unwrap()));
+
+    let mut group = c.benchmark_group("throughput-create-shares");
+    group.throughput(Throughput::Bytes(data.len() as u64));
+    group.bench_function("create_shares_1mb", |b| b.iter(|| sharer.share(black_box(data.clone()))));
+    group.finish();
 }
 
 criterion_group!(benches, ida_benchmark);
